4cd3740fd3c09b2206e75df5f35148f78b3b03c6
Administrator <Administrator@A-201611301328>
2017-06-09 11:32:58 +0800

    1

--- Gittest/src/test/Ta.java
+++ Gittest/src/test/Ta.java
@@ -4,10 +4,9 @@ public class Ta {
 	public int sum(int x,int y){
 		return x+y;
 	}
-	public void print(){
+	public void print(int x){
 		
 	}
 	public static void main(String[] args) {
-		Ta aTa=new Ta();
 	}
 }
